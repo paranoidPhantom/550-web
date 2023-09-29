3171a403ecda22bf461644c1bb59016e0636360b
Andrei <andrei.hudalla@outlook.com>
2023-09-29 15:08:21 -0500

    Added admin panel

---
+++ pages/admin/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+definePageMeta({
+    name: "Панель управления",
+    layout: "admin",
+    middleware: "auth",
+    login_message: "Для доступа к панели управления необходимо авторизоваться"
+})
+
+</script>
+
+<template>
+    
+</template>
+
+<style scoped lang="scss">
+
+</style>
