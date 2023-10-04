1ade28a0046344c504fbf2d3890914ab61c5e7e3
Andrei <andrei.hudalla@outlook.com>
2023-10-03 21:57:56 -0500

    Various improvements to content editor + showcase

---
+++ components/content/testComponent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const textColor = ref(`255,255,255`)
+
+const randomizeColor = () => {
+    const [r, g, b] = [Math.random() * 255, Math.random() * 255, Math.random() * 255]
+    textColor.value = `${r}, ${g}, ${b}`
+}
+</script>
+
+<template>
+    <div class="component">
+        <p :style="{ color: `rgb(${textColor})` }">Это тестовый компонент, такие вот специальные компоненты могут быть чем угодно!</p>
+        <UButton class="btn" @click="randomizeColor">Поменять цвет</UButton>
+        <div class="contents">
+            <slot />
+        </div>
+        <p>Возможности безграничны!</p>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.btn {
+    transition: all 0.3s;
+}
+.btn:active {
+    scale: 1.1;
+    translate: 1rem 0;
+}
+</style>
--- pages/admin/editor.vue
+++ pages/admin/editor.vue
@@ -4,64 +4,65 @@ import { computedAsync } from '@vueuse/core'
 import { parseMarkdown } from "@nuxtjs/mdc/dist/runtime"
 
 definePageMeta({
-    name: "Редактор",
-    layout: "admin",
-    middleware: "auth"
+	name: "Редактор",
+	layout: "admin",
+	// middleware: "auth"
 })
 
 const input = ref("")
 const preview = computedAsync(async () => {
-    const initialText = input.value
-    const tree = await parseMarkdown(initialText)
-
-    return tree
+	let initialText = input.value
+	const tree = await parseMarkdown(initialText)
+	return tree
 }, null)
 </script>
 
 <template>
-    <div class="__editor">
-        <UTextarea class="input" placeholder="Введите Markdown разметку..." v-model="input" />
-        <div class="line"></div>
-        <div class="preview">
-            <GlobalMarkdownForamatter>
-                <ContentRendererMarkdown v-if="preview" :value="preview" />
-                <div class="no-preview" style="opacity: 0.8;" v-if="input === ''">
-                    <h3 style="display: flex; align-items: center; gap: 0.5rem;">
-                        <Icon name="line-md:question-circle" /> Начните писать в поле и увидите изменения в этом окне
-                    </h3>
-                    <li>Чтобы добавить заголовок напишите: <blockquote># [Заголовок]</blockquote>
-                    </li>
-                    <li>Чтобы добавить элемент списка напишите: <blockquote>- [Элемент списка]</blockquote>
-                    </li>
-                </div>
-            </GlobalMarkdownForamatter>
-        </div>
-    </div>
+	<div class="__editor">
+		<UTextarea class="input" placeholder="Введите Markdown разметку..." v-model="input" />
+		<div class="line"></div>
+		<div class="preview">
+			<GlobalMarkdownForamatter>
+				<ContentRendererMarkdown v-if="preview" :value="preview" />
+				<div class="no-preview" style="opacity: 0.8;" v-if="input === ''">
+					<h3 style="display: flex; align-items: center; gap: 0.5rem;">
+						<Icon name="line-md:question-circle" /> Начните писать в поле и увидите изменения в этом окне
+					</h3>
+					<li>Чтобы добавить заголовок напишите: <blockquote># [Заголовок]</blockquote>
+					</li>
+					<li>Чтобы добавить элемент списка напишите: <blockquote>- [Элемент списка]</blockquote>
+					</li>
+				</div>
+			</GlobalMarkdownForamatter>
+		</div>
+	</div>
 </template>
 
 <style lang="scss">
 .__editor {
-    display: flex;
-    padding: 1rem;
-    gap: 1rem;
+	display: flex;
+	padding: 1rem;
+	gap: 1rem;
+	justify-content: space-between;
 
-    .line {
-        background-color: rgba(var(--inverted-rgb), 0.2);
-        width: 1px;
-    }
+	.line {
+		background-color: rgba(var(--inverted-rgb), 0.2);
+		width: 1px;
+	}
 
-    .input {
-        min-width: calc(50% - 2rem);
-        height: calc(100vh - 5rem);
+	.input {
+		min-width: calc(50% - 2rem);
+		height: calc(100vh - 5rem);
 
-        textarea {
-            height: inherit;
-        }
-    }
+		textarea {
+			height: inherit;
+		}
+	}
 
-    .preview {
-        min-width: 50%;
-        height: calc(100vh - 5rem);
-        overflow-y: auto;
-    }
-}</style>
+	.preview {
+		min-width: calc(50% - 2rem);
+		height: calc(100vh - 5rem);
+		overflow-y: auto;
+	}
+}
+</style>
