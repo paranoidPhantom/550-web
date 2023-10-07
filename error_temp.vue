59fc494113b2ce19f15f565b19e42da1134c2948
Andrei <andrei.hudalla@outlook.com>
2023-10-07 13:44:29 -0500

    Prepared error page for markup

---
+++ error_temp.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps(["error"])
+</script>
+
+<template>
+    <h1>{{ error.statusCode }}</h1>
+    
+</template>
+
+<style scoped lang="scss">
+
+h1 {
+    font-family: 'Martian Mono', monospace;
+}
+</style>
