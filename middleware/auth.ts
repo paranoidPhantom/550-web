8a7c8c27425681e7fd1d3e01032715dfe7ce8b19
Andrei <andrei.hudalla@outlook.com>
2023-10-10 17:03:02 -0500

    Added env variables for supabase client

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,7 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
     if (process.server) return
-    const { auth } = useSupabase()
+    const { public: { supabase_init_params } } = useRuntimeConfig()
+    const { auth } = useSupabase(supabase_init_params as unknown as init_params)
     const { data: { user }, error } = await auth.getUser()
     if (!user) { return navigateTo(`/auth/login?callback=${to.fullPath}&message=${to.meta.login_message}`) }
 })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,5 +27,14 @@ export default defineNuxtConfig({
 		preference: 'light', // default value of $colorMode.preference
 		fallback: 'light', // fallback value if not system preference found
 		storageKey: 'nuxt-color-mode'
+	},
+	runtimeConfig: {
+		public: {
+			supabase_init_params: {
+				host: process.env.supabase_host,
+				port: process.env.supabase_port,
+				key: process.env.supabase_anon_key
+			}
+		}
 	}
 })
