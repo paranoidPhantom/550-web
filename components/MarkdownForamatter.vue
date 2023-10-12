2df8e92f0f2aca0899a3186d72f111800edbfb6f
Andrei <andrei.hudalla@outlook.com>
2023-10-12 16:40:25 -0500

    Fine tuned Markdown formatting

--- components/MarkdownForamatter.vue
+++ components/MarkdownForamatter.vue
@@ -7,25 +7,55 @@
 <style lang="scss">
 .md-formatter {
     >div {
+        font-weight: 300;
         >* {
             margin: 1rem 0;
         }
 
+        color: rgba(var(--inverted-rgb), 0.8);
+
         >h1 {
             font-size: xx-large;
+            font-weight: 700;
+            color: rgba(var(--inverted-rgb), 1);
         }
 
         >h2 {
             font-size: x-large;
+            font-weight: 600;
+            color: rgba(var(--inverted-rgb), 1);
         }
 
         >h3 {
             font-size: larger;
+            font-weight: 500;
+            color: rgba(var(--inverted-rgb), 1);
         }
 
         >hr {
             opacity: 0.15;
             border-color: rgb(var(--inverted-rgb))
+        }
+
+        a {
+            color: rgb(var(--color-primary-DEFAULT));
+            text-decoration: underline 1px dotted;
+        }
+
+        h1 > a, h2 > a, h3 > a, h4 > a, h5 > a, h6 > a {
+            color: unset
+        }
+
+        table {
+            width: fit-content;
+            overflow: hidden;
+            border-radius: 1rem;
+            outline: 1px solid rgba(var(--inverted-rgb), 0.2);
+            th, td {
+                padding: 0.2rem 1rem;
+                border: 1px solid rgba(var(--inverted-rgb), 0.2);
+                text-align: center;
+            }
         }
 
         blockquote {
@@ -55,6 +85,10 @@
                     padding: 0 0.5rem;
                 }
             }
+        }
+        img {
+            border-radius: 0.5rem;
+            border: 2px solid rgba(var(--inverted-rgb), 0.1);
         }
     }
 }
