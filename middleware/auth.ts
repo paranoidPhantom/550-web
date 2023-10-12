3eb9d618bebdf40cb447fead1fbcab3e5a8b4a91
Andrei <andrei.hudalla@outlook.com>
2023-10-12 16:40:34 -0500

    Fixed middleware bug

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,4 +1,5 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
+    if (process.server) { return }
     const { auth } = useSupabaseClient()
     const { data: { user }, error } = await auth.getUser()
     if (!user) { return navigateTo(`/auth/login?callback=${to.fullPath}&message=${to.meta.login_message}`) }
