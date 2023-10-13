6f2ec9d4d915838a0ee86f8c5f3418c1c5ba40d9
Andrei <andrei.hudalla@outlook.com>
2023-10-13 09:08:37 -0500

    Added error page

---
+++ error.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps(["error"])
+</script>
+
+<template>
+    <div class="__error">
+        <h1>{{ error.statusCode }}</h1>
+        <h2>{{ error.message }}</h2>
+        <UButton to="/" label="На главную"/>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.__error {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    height: 100vh;
+    font-size: "Oswald", sans-serif;
+    h1 {
+        font-size: 4rem;
+        font-weight: 700;
+        color: rgb(var(--color-primary-DEFAULT));
+    }
+    h2 {
+        font-size: 2rem;
+        font-weight: 500;
+    }
+}
+</style>
