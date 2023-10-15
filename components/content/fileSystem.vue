42f526ca46f0ab722c3a0cbce743ca277e5f0f16
Andrei <andrei.hudalla@outlook.com>
2023-10-14 22:49:41 -0500

    Added fs component for content editor

---
+++ components/content/fileSystem.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type { props } from '../FolderViewer.vue';
+const open = ref(false)
+
+defineProps<props>()
+
+defineShortcuts({
+  escape: {
+    usingInput: true,
+    whenever: [open],
+    handler: () => { open.value = false }
+  }
+})
+</script>
+
+<template>
+    <div class="__fs">
+        <USlideover v-model="open" class="__fs-slideover">
+            <div class="component-container">
+                <FolderViewer :bucket="bucket" :root="root"/>
+            </div>
+        </USlideover>
+        <UButton color="white" @click="open = true">
+            <Icon name="line-md:clipboard-list-twotone"/>
+            Посмотреть файлы
+        </UButton>
+    </div>
+</template>
+
+<style lang="scss">
+.__fs-slideover {
+    .component-container {
+        .root {
+            margin: 1rem;
+            width: 100%;
+            --files-frame-height: calc(100vh - 4.5rem);
+        }
+    }
+}
+</style>
