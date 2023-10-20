b3799ffd9da55a0f6c00a136cac7122b36bc4e8c
Andrei <andrei.hudalla@outlook.com>
2023-10-20 12:38:52 -0500

    Added news editing functionality

--- pages/admin/editor.vue
+++ pages/admin/editor.vue
@@ -1,21 +1,33 @@
 <script setup lang="ts">
 import { computedAsync } from "@vueuse/core";
 import { parseMarkdown } from "@nuxtjs/mdc/dist/runtime";
-import type { PostgrestError } from "@supabase/supabase-js";
-import nuxtStorage from 'nuxt-storage';
+import type {
+    PostgrestError,
+    RealtimePostgresChangesPayload,
+} from "@supabase/supabase-js";
+import nuxtStorage from "nuxt-storage";
+import { content } from "#tailwind-config";
+
+interface article {
+    route: string;
+    created_at: string;
+    thumbnail: string;
+    link_text: string;
+}
 
 definePageMeta({
     name: "Редактор",
     layout: "admin",
     middleware: "auth",
-    login_message: "Для доступа к редактору контента необходимо авторизоваться"
+    login_message: "Для доступа к редактору контента необходимо авторизоваться",
 });
-
 
 const supabase = useSupabaseClient();
 const toast = useToast();
 
-const tableName = "content";
+const {
+    tableNames: { content: tableName, news: newsTableName },
+} = useAppConfig();
 
 const input = ref();
 const initialInput = ref("");
@@ -26,11 +38,17 @@ const preview = computedAsync(async () => {
 }, null);
 
 const handleDBError = (error: PostgrestError | null) => {
-    if (!error) { return }
-    let errorDescription = `${error.message}`
+    if (!error) {
+        return;
+    }
+    let errorDescription = `${error.message}`;
     switch (parseInt(error.code)) {
         case 23505:
-            errorDescription = "Страница с этим путём или названием уже существует!"
+            errorDescription =
+                "Страница с этим путём или названием уже существует!";
+            break;
+        case 42501:
+            errorDescription = `Недостаточно прав (${error.message})`;
             break;
     }
     toast.add({
@@ -41,13 +59,12 @@ const handleDBError = (error: PostgrestError | null) => {
         color: "red",
         timeout: 10000,
     });
-}
-
+};
 
 // Handling content_pages reactivity
 const content_pages: Ref<page[]> = ref([]);
-const { data, error } = await supabase.from(tableName).select();
-handleDBError(error)
+const { data, error } = await supabase.from(tableName).select("*, news(*)");
+handleDBError(error);
 content_pages.value = data as page[];
 
 interface page {
@@ -55,7 +72,51 @@ interface page {
     created_at: string;
     name: string;
     route: string;
+    news: article;
 }
+
+const handle_postgres_changes = (
+    payload: RealtimePostgresChangesPayload<{ [key: string]: any }>
+) => {
+    if (payload.table === tableName) {
+        switch (payload.eventType) {
+            case "INSERT": {
+                content_pages.value.push(payload.new as page);
+                break;
+            }
+            case "UPDATE": {
+                content_pages.value.forEach((page: page, index: number) => {
+                    if (page.route === (payload.new as page).route) {
+                        const newsPreserve = page.news;
+                        content_pages.value[index] = payload.new as page;
+                        content_pages.value[index].news = newsPreserve;
+                    }
+                });
+                break;
+            }
+            case "DELETE": {
+                content_pages.value.forEach((page: page, index: number) => {
+                    if (page.route === (payload.old as page).route) {
+                        content_pages.value = content_pages.value
+                            .slice(0, index)
+                            .concat(content_pages.value.slice(index + 1));
+                    }
+                });
+                current_route.value = content_pages.value[0];
+                break;
+            }
+        }
+    } else if (payload.table === newsTableName) {
+        let { route: editedRoute } = payload.old as { route: string };
+        if (!editedRoute)
+            editedRoute = (payload.new as { route: string }).route;
+        content_pages.value.forEach((page: page, index: number) => {
+            if (page.route === editedRoute) {
+                content_pages.value[index].news = payload.new as article;
+            }
+        });
+    }
+};
 
 const contentRealtime = supabase
     .channel("custom-all-channel")
@@ -66,35 +127,16 @@ const contentRealtime = supabase
             schema: "public",
             table: tableName,
         },
-        (payload) => {
-            switch (payload.eventType) {
-                case "INSERT": {
-                    content_pages.value.push(payload.new as page);
-                    break;
-                }
-                case "UPDATE": {
-                    content_pages.value.forEach((page: page, index: number) => {
-                        if (page.route === (payload.new as page).route) {
-                            content_pages.value[index] = payload.new as page;
-                        }
-                    });
-                    break;
-                }
-                case "DELETE": {
-                    content_pages.value.forEach((page: page, index: number) => {
-                        if (page.route === (payload.old as page).route) {
-                            content_pages.value = content_pages.value
-                                .slice(0, index)
-                                .concat(content_pages.value.slice(index + 1));
-                        }
-                    });
-                    current_route.value = content_pages.value[0]
-                    break;
-                }
-                default:
-                    break;
-            }
-        }
+        handle_postgres_changes
+    )
+    .on(
+        "postgres_changes",
+        {
+            event: "*",
+            schema: "public",
+            table: newsTableName,
+        },
+        handle_postgres_changes
     )
     .subscribe();
 
@@ -120,7 +162,10 @@ const content_routes_list = computed(() => {
         result.push(option_object);
         if (lastHref && lastHref === page.route) {
             current_route.value = option_object;
-        } else if (!current_route.value && index === content_pages.value.length - 1) {
+        } else if (
+            !current_route.value &&
+            index === content_pages.value.length - 1
+        ) {
             current_route.value = option_object;
         }
     });
@@ -128,7 +173,6 @@ const content_routes_list = computed(() => {
     return result;
 });
 // Done with content_pages
-
 
 const firstLoadComplete = ref(false);
 
@@ -140,9 +184,14 @@ watchEffect(async () => {
             .select()
             .eq("route", request_href)
             .single();
-        if (error) { handleDBError(error); return }
+        if (error) {
+            handleDBError(error);
+            return;
+        }
         const loadedVal = (data as unknown as page).content;
-        const editing_value_cookie = nuxtStorage.localStorage.getData('editing_value_cookie')
+        const editing_value_cookie = nuxtStorage.localStorage.getData(
+            "editing_value_cookie"
+        );
         input.value =
             editing_route_cookie.value === request_href
                 ? editing_value_cookie || loadedVal
@@ -150,8 +199,8 @@ watchEffect(async () => {
         editing_route_cookie.value = request_href;
         initialInput.value = data ? loadedVal : "";
         if (!firstLoadComplete.value) firstLoadComplete.value = true;
-        input.value = input.value.toString()
-        initialInput.value = initialInput.value.toString()
+        input.value = input.value.toString();
+        initialInput.value = initialInput.value.toString();
     }
 });
 
@@ -187,8 +236,11 @@ const confirmPublishChanges = async () => {
         .from(tableName)
         .update({ content: input.value })
         .eq("route", current_route.value.href);
-    if (error) { handleDBError(error); return }
-    nuxtStorage.sessionStorage.removeItem('editing_value_cookie');
+    if (error) {
+        handleDBError(error);
+        return;
+    }
+    nuxtStorage.sessionStorage.removeItem("editing_value_cookie");
     initialInput.value = input.value;
     toast.add({
         id: `publish_complete_${input.value}`,
@@ -212,18 +264,25 @@ const pageCreateFields = reactive(settingsFieldsDefaults);
 const settingsFields = reactive(settingsFieldsDefaults);
 
 const createPage = async () => {
-    if (pageCreateFields.name === "" || pageCreateFields.route === "") { return }
-        pageCreateOpen.value = false;
-    const { error } = await supabase.from(tableName).insert({
+    if (pageCreateFields.name === "" || pageCreateFields.route === "") {
+        return;
+    }
+    pageCreateOpen.value = false;
+    const { error } = await supabase.from(tableName as any).insert({
         route: pageCreateFields.route,
         name: pageCreateFields.name,
-        content: `::editorWelcome\n---\nparams: ${JSON.stringify(pageCreateFields)}\n---\n::`,
-    });
-    if (error) { handleDBError(error); return }
+        content: `::editorWelcome\n---\nparams: ${JSON.stringify(
+            pageCreateFields
+        )}\n---\n::`,
+    } as any);
+    if (error) {
+        handleDBError(error);
+        return;
+    }
     current_route.value = {
         href: pageCreateFields.route,
-        name: pageCreateFields.name
-    }
+        name: pageCreateFields.name,
+    };
     pageCreateFields.name = "";
     pageCreateFields.route = "";
 };
@@ -235,7 +294,10 @@ const updatePageField = async (field: string, value: any) => {
         .from(tableName)
         .update(setVal)
         .eq("route", current_route.value.href);
-    if (error) { handleDBError(error); return }
+    if (error) {
+        handleDBError(error);
+        return;
+    }
     settingsFields[field as keyof typeof settingsFieldsDefaults] = "";
     slideoverOpen.value = false;
 };
@@ -268,35 +330,42 @@ const confirmDeletePage = async () => {
         .from(tableName)
         .delete()
         .eq("route", current_route.value.href);
-    if (error) { handleDBError(error); return }
+    if (error) {
+        handleDBError(error);
+        return;
+    }
 
-    current_route.value = content_routes_list.value[0]
+    current_route.value = content_routes_list.value[0];
 };
 
+const currentInput = useState("editor_input_global", () => undefined);
 
-const currentInput = useState("editor_input_global", () => undefined)
-
-const editor = ref()
-const textArea: Ref<HTMLTextAreaElement | undefined> = ref()
-const selectionIndex = ref(0)
+const editor = ref();
+const textArea: Ref<HTMLTextAreaElement | undefined> = ref();
+const selectionIndex = ref(0);
 
 const updateSI = () => {
-    selectionIndex.value = textArea.value ? textArea.value.selectionEnd : 0
-}
+    selectionIndex.value = textArea.value ? textArea.value.selectionEnd : 0;
+};
 
 watchEffect(() => {
     if (currentInput.value) {
-        input.value = currentInput.value
-        updateSI()
+        input.value = currentInput.value;
+        updateSI();
     }
-})
+});
 
 watchEffect(() => {
     if (input.value === undefined) {
         return;
     }
-    nuxtStorage.localStorage.setData('editing_value_cookie', input.value, 24, 'h');
-    updateSI()
+    nuxtStorage.localStorage.setData(
+        "editing_value_cookie",
+        input.value,
+        24,
+        "h"
+    );
+    updateSI();
 });
 
 defineShortcuts({
@@ -316,18 +385,95 @@ defineShortcuts({
 });
 
 onMounted(() => {
-    const root = editor.value
-    const textAreaSelection = root.querySelector("textarea")
-    textArea.value = textAreaSelection
+    const root = editor.value;
+    const textAreaSelection = root.querySelector("textarea");
+    textArea.value = textAreaSelection;
     //
     textArea.value?.addEventListener("keyup", updateSI),
-    textArea.value?.addEventListener("click", updateSI)
-})
+        textArea.value?.addEventListener("click", updateSI);
+});
 
 onUnmounted(() => {
     textArea.value?.removeEventListener("keyup", updateSI),
-    textArea.value?.removeEventListener("click", updateSI)
-})
+        textArea.value?.removeEventListener("click", updateSI);
+});
+
+const currentPageData = computed(() => {
+    const currentHref = current_route.value.href;
+    const pages = content_pages.value;
+    let result: page | undefined;
+    pages.forEach((page: page, index: number) => {
+        if (page.route === currentHref) {
+            result = page;
+        }
+    });
+    return result;
+});
+
+const associateNewsEntryPending = ref(false);
+
+const associateNewsEntry = async () => {
+    associateNewsEntryPending.value = true;
+    const { error } = await supabase.from(newsTableName as any).insert({
+        route: current_route.value.href,
+    } as any);
+    if (error) {
+        handleDBError(error);
+        associateNewsEntryPending.value = false;
+        return;
+    }
+};
+
+const editNewsEntry = reactive({
+    thumbnail: "",
+    link_text: "",
+});
+
+const openSlideover = () => {
+    slideoverOpen.value = true;
+    if (currentPageData.value && currentPageData.value.news) {
+        editNewsEntry.thumbnail = currentPageData.value?.news.thumbnail;
+        editNewsEntry.link_text = currentPageData.value?.news.link_text;
+    }
+};
+
+const updateNewsEntry = async (key: keyof typeof editNewsEntry) => {
+    const setTo = {};
+    setTo[key] = editNewsEntry[key];
+    const { error } = await supabase
+        .from(newsTableName as any)
+        .update(setTo)
+        .eq("route", current_route.value.href)
+        .select();
+    if (error) {
+        handleDBError(error);
+        associateNewsEntryPending.value = false;
+        return;
+    }
+};
+
+const news_edit_settings: {
+    label: string;
+    name: string;
+    help: string;
+    placeholder: string;
+    key: keyof typeof editNewsEntry;
+}[] = [
+    {
+        label: "Обложка",
+        name: "thumbnail",
+        help: "Ссылка на картинку обложку для новости",
+        placeholder: "https://example.com/image.png",
+        key: "thumbnail",
+    },
+    {
+        label: "Текст ссылки",
+        name: "link_text",
+        help: "Текст для ссылки (в форме кнопки) на новость",
+        placeholder: "Подробнее",
+        key: "link_text",
+    },
+];
 </script>
 
 <template>
@@ -368,6 +514,7 @@ onUnmounted(() => {
         </UModal>
         <USlideover v-model="slideoverOpen">
             <div class="__editor-slideover">
+                <h1 class="font-bold">Основная информация</h1>
                 <UFormGroup
                     label="Путь к странице"
                     name="route"
@@ -394,6 +541,78 @@ onUnmounted(() => {
                         settingsFields.name === current_route.label
                     "
                     @click="updatePageField('name', settingsFields.name)"
+                />
+                <hr />
+                <template
+                    v-if="
+                        current_route &&
+                        current_route.href &&
+                        current_route.href.startsWith('/news')
+                    "
+                >
+                    <h1 class="font-bold">Новостная страница</h1>
+                    <template v-if="currentPageData && !currentPageData.news">
+                        <h2>У этой страницы нет привязанной новости</h2>
+                        <UButton
+                            :loading="associateNewsEntryPending"
+                            :disabled="associateNewsEntryPending"
+                            label="Создать"
+                            variant="soft"
+                            @click="associateNewsEntry"
+                        />
+                    </template>
+                    <template
+                        v-else-if="currentPageData && currentPageData.news"
+                    >
+                        <UFormGroup
+                            v-for="edit_setting in news_edit_settings"
+                            :label="edit_setting.label"
+                            :name="edit_setting.name"
+                            :help="edit_setting.help"
+                        >
+                            <UInput
+                                v-model="editNewsEntry[edit_setting.key]"
+                                :placeholder="edit_setting.placeholder"
+                                :ui="{
+                                    icon: { trailing: { pointer: '' } },
+                                }"
+                            >
+                                <template
+                                    #trailing
+                                    v-if="
+                                        editNewsEntry[edit_setting.key] !==
+                                        currentPageData.news[edit_setting.key]
+                                    "
+                                >
+                                    <UButton
+                                        variant="link"
+                                        icon="i-heroicons-arrow-path-rounded-square"
+                                        @click="
+                                            editNewsEntry[edit_setting.key] =
+                                                currentPageData.news[
+                                                    edit_setting.key
+                                                ]
+                                        "
+                                    />
+                                    <UButton
+                                        variant="link"
+                                        icon="i-heroicons-check-20-solid"
+                                        @click="
+                                            updateNewsEntry(edit_setting.key)
+                                        "
+                                    />
+                                </template>
+                            </UInput>
+                        </UFormGroup>
+                    </template>
+                </template>
+                <UAlert
+                    v-else
+                    icon="i-heroicons-information-circle-20-solid"
+                    description="Для того чтобы привязывать к страницам новость, путь страницы должен начинаться на '/news/...'"
+                    title="Примечание"
+                    color="yellow"
+                    variant="soft"
                 />
                 <hr />
                 <h1 class="font-bold text-red-500 dark:text-red-400">
@@ -424,7 +643,7 @@ onUnmounted(() => {
                     :disabled="input !== initialInput || !firstLoadComplete"
                 />
                 <UButton
-                    @click="slideoverOpen = !slideoverOpen"
+                    @click="openSlideover"
                     class="edit-page"
                     color="white"
                     :disabled="!firstLoadComplete"
@@ -433,7 +652,10 @@ onUnmounted(() => {
                 <UButton :to="current_route.href" target="_blank" color="white"
                     ><Icon name="ic:baseline-remove-red-eye"
                 /></UButton>
-                <AdminUtilityEditorAutocomplete :input="input" :index="selectionIndex" />
+                <AdminUtilityEditorAutocomplete
+                    :input="input"
+                    :index="selectionIndex"
+                />
                 <Icon
                     name="svg-spinners:ring-resize"
                     class="load-spinner"
