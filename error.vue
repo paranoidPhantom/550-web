eea22008552ab9b60840ac8f45f58393facc5c93
Andrei <andrei.hudalla@outlook.com>
2023-11-05 17:59:02 -0500

    Visual improvement for error page

--- error.vue
+++ error.vue
@@ -11,29 +11,67 @@ switch (error.statusCode) {
 
 <template>
     <div class="__error">
-        <h1>{{ error.statusCode }} {{ suffix ? `(${suffix})` : "" }}</h1>
-        <h2>{{ error.message }}</h2>
-        <UButton to="/" label="На главную" />
+        <div class="error-wrapper">
+            <img src="/images/error.svg" alt="broken robot">
+            <h1>{{ error.statusCode }} {{ suffix ? `(${suffix})` : "" }}</h1>
+            <h2>{{ error.message }}</h2>
+            <UButton to="/" label="На главную" />
+        </div>
     </div>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
+@keyframes gradient {
+	0% {
+		background-position: 0% 50%;
+	}
+	100% {
+		background-position: 100% 50%;
+	}
+}
+@keyframes text {
+    5% {
+        color: rgb(var(--color-primary-DEFAULT));
+    }
+	50% {
+        color: rgb(var(--normal-rgb));
+	}
+    95% {
+        color: rgb(var(--color-primary-DEFAULT));
+    }
+}
+
 .__error {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 2rem;
-    height: 100vh;
-    font-size: "Oswald", sans-serif;
+    --anim-time: 5s;
+	background: linear-gradient(-45deg, rgba(var(--normal-rgb), 0.5), rgba(var(--normal-rgb)), rgba(var(--normal-rgb)), rgba(var(--color-primary-DEFAULT)), rgba(var(--normal-rgb)), rgba(var(--normal-rgb)), rgba(var(--normal-rgb)));
+	background-size: 300% 300%;
+	animation: gradient var(--anim-time) ease-in-out alternate infinite;
+	height: 100vh;
+    position: relative;
+    img {
+        width: 50vh;
+    }
     h1 {
         font-size: 4rem;
         font-weight: 700;
         color: rgb(var(--color-primary-DEFAULT));
+	    animation: text var(--anim-time) ease-in-out infinite;
     }
     h2 {
         font-size: 2rem;
         font-weight: 500;
+    }
+    .error-wrapper {
+        position: absolute;
+        inset: 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 2rem;
+        font-size: "Oswald", sans-serif;
+        backdrop-filter: blur(5rem);
+        z-index: 3;
     }
 }
 </style>
