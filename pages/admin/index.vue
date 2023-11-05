56808aa9c3c96e6948fcfb573dcf64080ec309f2
Andrei <andrei.hudalla@outlook.com>
2023-11-05 18:00:06 -0500

    New admin page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useFormatedDate } from "@/utils/useFormatedDate";
+import { SupabaseClient } from "@supabase/supabase-js";
 definePageMeta({
     name: "Панель управления",
     layout: "admin",
@@ -7,190 +8,80 @@ definePageMeta({
     login_message: "Для доступа к панели управления необходимо авторизоваться",
 });
 
-const options = [
+const links = [
     {
-        label: "Изображения",
-        id: "images",
+        label: "Страницы",
+        detail: "Список страниц с контентом",
+        icon: "i-heroicons-list-bullet-20-solid",
+        perm_required: "can_edit_content",
+        href: "/admin/pages",
     },
     {
-        label: "Документы",
-        id: "docs",
+        label: "Редактор",
+        detail: "Редактирование страниц с контентом",
+        icon: "i-heroicons-pencil-square-solid",
+        perm_required: "can_edit_content",
+        href: "/admin/editor",
     },
     {
-        label: "Другое",
-        id: "misc",
-    },
-];
-
-const columns = [
-    {
-        key: "article_thumbnail",
+        label: "Блог",
+        detail: "Редактирование личного блога",
+        icon: "i-heroicons-chat-bubble-bottom-center-text",
+        perm_required: "can_edit_blog",
+        href: "/admin/blog",
     },
     {
-        key: "name",
-        label: "Название",
-        sortable: true
-    },
-    {
-        key: "route",
-        label: "Путь",
-        sortable: true
-    },
-    {
-        key: "created_at",
-        label: "Создана",
-        sortable: true
-    },
-    {
-        key: "actions",
+        label: "Пользователи",
+        detail: "Список пользователей и их редактирование",
+        icon: "i-heroicons-user-group-20-solid",
+        perm_required: "can_edit_users",
+        href: "/admin/users",
     },
 ];
 
-const {
-    tableNames: { content: tableName, news: newsTableName },
-} = useAppConfig();
+const supabase = useSupabaseClient()
 
-const supabase = useSupabaseClient();
+const perms = ref();
+const user_email = ref()
 
-const folderViewerBucket = ref(options[0]);
-
-const { push } = useRouter();
-
-const { data: pages } = await supabase
-    .from(tableName)
-    .select(`route, created_at, name, ${newsTableName}(thumbnail)`);
-
-const formattedPages = computed(() => {
-    let result: any[] = [];
-    pages?.map((page: any, index: number) => {
-        let correspondingObject = page;
-        correspondingObject.article_thumbnail = page.news
-            ? page.news.thumbnail
-            : null;
-        correspondingObject.news = undefined;
-        correspondingObject.created_at = page.created_at;
-        result[index] = correspondingObject;
-    });
-    return result;
-});
-
-const q = ref('')
-const filteredRows = computed(() => {
-  if (!q.value) {
-    return formattedPages.value
-  }
-  return formattedPages.value.filter((page) => {
-    return Object.values(page).some((value) => {
-      return String(value).toLowerCase().includes(q.value.toLowerCase())
-    })
-  })
+onMounted(async () => {
+    const { data: { user } } = await supabase.auth.getUser()
+    user_email.value = user?.email
+    perms.value = user?.app_metadata;
 })
 </script>
 
 <template>
     <div class="__admin-home">
-        <div class="fs-wrapper">
-            <USelectMenu v-model="folderViewerBucket" :options="options">
-                <UAlert
-                    :title="folderViewerBucket.label"
-                    variant="subtle"
-                    icon="i-heroicons-command-line"
-                    color="primary"
+        <template v-for="link in links">
+            <UTooltip
+                key="link.href"
+                v-if="
+                    (perms && perms[link.perm_required] === true) ||
+                    user_email === 'root@ort'
+                "
+                :text="link.detail"
+            >
+                <UButton
+                    :icon="link.icon"
+                    :label="link.label"
+                    :to="link.href"
+                    size="xl"
+                    color="white"
                 />
-            </USelectMenu>
-            <Icon name="i-heroicons-arrow-down-solid" class="arrow" />
-            <FolderViewer
-                class="fs full"
-                :bucket="folderViewerBucket.id"
-                root=""
-            />
-        </div>
-        <div class="divider" />
-        <div class="page-list">
-            <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
-                <UInput v-model="q" placeholder="Поиск..." />
-            </div>
-            <UTable style="width: 100%;" :rows="filteredRows" :columns="columns">
-                <template #article_thumbnail-data="{ row }">
-                    <img
-                        class="article-thumbnail"
-                        :src="row.article_thumbnail"
-                        alt=""
-                        v-if="row.article_thumbnail"
-                    />
-                    <UPopover v-else mode="hover">
-                        <Icon
-                            style="width: 30%; margin: 0 35%; height: 100%"
-                            :name="
-                                row.article_thumbnail === null
-                                    ? `i-heroicons-x-mark-solid`
-                                    : `i-streamline-interface-layout-border-none-border-cell-format-formatting-none`
-                            "
-                        />
-                        <template #panel>
-                            <p class="p-2">
-                                {{
-                                    row.article_thumbnail === null
-                                        ? `У страницы нет ассоциированной статьи`
-                                        : `У статьи нет ассоциированной картинки`
-                                }}
-                            </p>
-                        </template>
-                    </UPopover>
-                </template>
-                <template #created_at-data="{ row }">
-                    {{ useFormatedDate(row.created_at) }}
-                </template>
-                <template #actions-data="{ row }">
-                    <UButton
-                        @click="
-                            push({
-                                path: '/admin/editor',
-                                query: { editing: row.route },
-                            })
-                        "
-                        icon="i-heroicons-pencil-square"
-                        variant="ghost"
-                    />
-                </template>
-            </UTable>
-        </div>
+            </UTooltip>
+        </template>
     </div>
 </template>
 
 <style scoped lang="scss">
 .__admin-home {
     display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: calc(100vh - 3rem);
     gap: 1rem;
     padding: 1rem;
-    .article-thumbnail {
-        width: 8rem;
-        border-radius: 0.5rem;
-    }
-    .divider {
-        width: 0;
-        border-right: 1px solid rgba(255, 255, 255, 0.1);
-    }
-    .page-list {
-        width: 100%;
-        max-height: calc(100vh - 5rem);;
-        overflow-y: auto;
-    }
-    .fs-wrapper {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-        width: 25rem;
-        flex-shrink: 0;
-        height: calc(100vh - 5rem);
-        .arrow {
-            font-size: 1.5rem;
-            width: 100%;
-        }
-        .fs {
-            max-width: unset;
-            --files-frame-height: 20rem;
-        }
-    }
 }
 </style>
