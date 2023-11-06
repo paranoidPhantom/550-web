aa26c55fe2ec4798bc9d11f7791a1beeaed8b39a
Andrei <andrei.hudalla@outlook.com>
2023-11-06 10:29:48 -0600

    Added more status indication for server middleware

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -5,11 +5,15 @@ export default defineEventHandler(async (event) => {
     if (!getRequestURL(event).pathname.startsWith("/api/admin")) return
     const supabase = await serverSupabaseServiceRole(event)
     const access_token = event.headers.get("access_token")
+    let progress = 0
     if (access_token) {
         const { data: { user } } = await supabase.auth.getUser(access_token) as UserResponse
+        progress++
         if (user) {
+            progress++
             const { data: claims } = await supabase.rpc("get_claims", { uid: user.id } as any)
             if (claims) {
+                progress++
                 if (user.email === "root@ort" || claims["can_edit_users"] === true) {
                     event.context.authenticated = true
                     return
@@ -22,6 +26,6 @@ export default defineEventHandler(async (event) => {
     }
     throw createError({
         statusCode: 401,
-        statusMessage: 'Insufficient rights',
+        message: 'Insufficient rights - '+progress,
     })
 })
