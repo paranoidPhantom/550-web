fa7209dca461ea74705f44d5470b0e7e320df337
Andrei <andrei.hudalla@outlook.com>
2023-11-06 11:06:50 -0600

    Update auth.ts

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,31 +1,24 @@
 import { serverSupabaseServiceRole } from '#supabase/server'
-import type { User, UserResponse } from '@supabase/supabase-js'
+import type { UserResponse } from '@supabase/supabase-js'
 
 export default defineEventHandler(async (event) => {
     if (!getRequestURL(event).pathname.startsWith("/api/admin")) return
     const supabase = await serverSupabaseServiceRole(event)
     const access_token = event.headers.get("access_token")
-    let progress = 0
     if (access_token) {
         const { data: { user } } = await supabase.auth.getUser(access_token) as UserResponse
-        progress++
         if (user) {
-            progress++
             const { data: claims } = await supabase.rpc("get_claims", { uid: user.id } as any)
             if (claims) {
-                progress++
                 if (user.email === "root@ort" || claims["can_edit_users"] === true) {
                     event.context.authenticated = true
                     return
                 }
-                console.log("Claims fetched")
             }
-            console.log("Access token valid | Got user")
         }
-        console.log("Got access_token")
     }
     throw createError({
         statusCode: 401,
-        message: 'Insufficient rights - '+progress,
+        message: 'Insufficient rights'
     })
 })
