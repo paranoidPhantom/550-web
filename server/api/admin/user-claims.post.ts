98ed153273417adbce6085c7edaf73c3c2b48f73
Andrei <andrei.hudalla@outlook.com>
2023-11-06 12:43:23 -0600

    Fixed perm toggle bug (yet again)

--- server/api/admin/user-claims.post.ts
+++ server/api/admin/user-claims.post.ts
@@ -5,6 +5,7 @@ export default defineEventHandler(
 		const supabase = await serverSupabaseServiceRole(event)
 		if (event.context.authenticated) {
 			const body = await readBody(event)
+			body.value = body.value === "true"
 			await supabase.rpc("set_claim", body as any)
 		}
 	}
