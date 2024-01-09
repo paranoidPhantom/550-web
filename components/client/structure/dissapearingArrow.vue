a0390f6b518a972ffd13c5b5679fcae504d634e6
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-01-09 19:03:47 +0300

    Improved disappearing arrow on home page

--- components/client/structure/dissapearingArrow.vue
+++ components/client/structure/dissapearingArrow.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
-import type { _opacity } from '#tailwind-config/theme';
+import { useWindowSize } from '@vueuse/core'
+
+const { width, height } = useWindowSize()
 
 const scroll = reactive({
     px: 0
@@ -8,6 +10,8 @@ const scroll = reactive({
 const updateScrollPos = () => {
     scroll.px = window.scrollY
 }
+
+const windowWidth = 
 
 onMounted(() => {
     window.addEventListener("scroll", updateScrollPos)
@@ -21,7 +25,8 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-    <Icon :style="{ opacity: 1 - scroll.px / 100 }" class="arrow" name="heroicons:arrow-small-down-solid"/>
+    <Icon :style="{ opacity: 1 - scroll.px / 100 }" class="arrow" :name="width > 800 ? 'heroicons:arrow-small-down-solid' : 'ic:baseline-swipe-up'"/>
+	<div v-if="width <= 800" class="round-hero-fade" :style="{ opacity: 1 - scroll.px / 100 }"></div>
 </template>
 
 <style lang="scss" scoped>
@@ -30,5 +35,23 @@ onBeforeUnmount(() => {
     left: 1rem;
     bottom: 1rem;
     font-size: 2rem;
+}
+
+.round-hero-fade {
+	position: absolute;
+	pointer-events: none;
+	inset: 0;
+	top: -6rem;
+	border-radius: 2.05rem;
+	box-shadow: 0 0 2rem 2rem rgba(var(--color-primary-DEFAULT)/0.8);
+	z-index: 2;
+}
+
+@media (max-width: 800px) {
+	.arrow {
+		left: unset;
+		width: 100%;
+	    bottom: calc(20vh - 8rem);
+	}
 }
 </style>
