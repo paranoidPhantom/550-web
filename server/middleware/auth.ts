9a8387a306348a89fb89bc5a617b603db5702c27
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-02-25 14:36:12 +0300

    Update auth.ts

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -19,6 +19,6 @@ export default defineEventHandler(async (event) => {
     }
     throw createError({
         statusCode: 401,
-        message: 'Insufficient rights - '+JSON.stringify(event.headers)
+        message: 'Insufficient rights - '+JSON.stringify(event.headers.get("access_token"))
     })
 })
