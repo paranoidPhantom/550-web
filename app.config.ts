d1fa1cbb61ab6d8bcc0895cc87a26ac9d84b65c1
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-03-10 11:15:55 +0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,30 +1,30 @@
 export default defineAppConfig({
-    website_name: "Школа №550",
-    ui: {
-        primary: 'sky',
-        gray: 'neutral'
-    },
-    socialLinks: [
-        {
-            icon: "i-mdi-vk",
-            link: "https://vk.com/school_550_spb"
-        },
-        {
-            icon: "i-mdi-telegram",
-            link: "https://t.me/school550_spb"
-        }
-    ],
-    tableNames: {
-        content: "content",
-        news: "news",
-        blog: "blogs"
-    },
-    home_bubble_text: [
-        "Современное оборудование",
-        "Современное оборудование",
-        "Современное оборудование",
-        "Современное оборудование",
-        "Современное оборудование",
-        "Современное оборудование"
-    ]
+	website_name: "Школа №550",
+	ui: {
+		primary: 'sky',
+		gray: 'neutral'
+	},
+	socialLinks: [
+		{
+			icon: "i-mdi-vk",
+			link: "https://vk.com/school_550_spb"
+		},
+		{
+			icon: "i-mdi-telegram",
+			link: "https://t.me/school550_spb"
+		}
+	],
+	tableNames: {
+		content: "content",
+		news: "news",
+		blog: "blogs"
+	},
+	home_bubble_text: [
+		"Современное оборудование",
+		"Современное оборудование",
+		"Современное оборудование",
+		"Современное оборудование",
+		"Современное оборудование",
+		"Современное оборудование"
+	]
 });
