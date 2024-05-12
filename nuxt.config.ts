2ce600d1071a831655313f6f99d5d46d9a2bc0c9
Andrei <andrei.hudalla@outlook.com>
2024-05-12 12:51:33 -0500

    Set up builds by removing SEO module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
-	sitemap: {
-		sources: [
-			'/api/public/_sitemap-urls'
-		],
-		xslTips: false
+	nitro: {
+		preset: 'node-server'
 	},
-	modules: ['@nuxtjs/turnstile', '@nuxt/ui', '@nuxt/content', 'nuxt-icon', '@nuxtjs/supabase', '@nuxtjs/mdc', '@nuxtjs/robots', '@nuxtjs/sitemap'],
+	devtools: { enabled: true },
+	modules: [
+		'@nuxt/ui',
+		'@nuxt/content',
+		'nuxt-icon',
+		'@nuxtjs/supabase',
+		'@nuxtjs/mdc'
+	],
 	app: {
 		pageTransition: { name: 'page', mode: 'out-in' },
 		layoutTransition: { name: 'page', mode: 'out-in' },
 	},
-	turnstile: {
-		siteKey: '0x4AAAAAAAP-6VsidBtdKXE8',
-		addValidateEndpoint: true
-	},
+	css: [
+		"assets/global.scss"
+	],
 	routeRules: {
 		"/docs": {
 			redirect: "/docs/base/intro"
@@ -36,9 +38,6 @@ export default defineNuxtConfig({
 			anchorLinks: false
 		}
 	},
-	css: [
-		"assets/global.scss"
-	],
 	colorMode: {
 		preference: 'light', // default value of $colorMode.preference
 		fallback: 'light', // fallback value if not system preference found
@@ -52,10 +51,16 @@ export default defineNuxtConfig({
 			Disallow: ["/admin/", "/docs/"]
 		}
 	},
+	sitemap: {
+		sources: [
+			'/api/public/_sitemap-urls'
+		],
+		xslTips: false
+	},
 	runtimeConfig: {
 		public: {
-			// access: process.env.ACCESS,
-			// build_id: process.env.BUILD_ID,
+			access: process.env.ACCESS,
+			build_id: process.env.BUILD_ID,
 			service_domain: process.env.SUPABASE_URL?.split(":8000")[0].split("://")[1]
 		},
 	}
