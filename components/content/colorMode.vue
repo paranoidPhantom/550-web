be9e94a8e72f5e8270d0f31b339b5a8083cd9d25
Andrei <andrei.hudalla@outlook.com>
2024-05-23 10:14:08 -0500

    Formatting - #87

--- components/content/colorMode.vue
+++ components/content/colorMode.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-const colorMode = useColorMode()
+const colorMode = useColorMode();
 </script>
 
 <template>
-    <slot v-if="colorMode.value === 'light'" name="light"/>
-    <slot v-else-if="colorMode.value === 'dark'" name="dark"/>
+    <slot v-if="colorMode.value === 'light'" name="light" />
+    <slot v-else-if="colorMode.value === 'dark'" name="dark" />
 </template>
