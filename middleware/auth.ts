5b01c71b47e0f4ceea087520546cd4c65eee1884
Andrei <andrei.hudalla@outlook.com>
2024-05-26 12:58:24 -0500

    Update auth.ts

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -3,4 +3,4 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 	const { auth } = useSupabaseClient()
 	const { data: { user }, error } = await auth.getUser()
 	if (!user) { return navigateTo(`/auth/login?callback=${to.fullPath}&message=${to.meta.login_message}`) }
-})
+}) 
