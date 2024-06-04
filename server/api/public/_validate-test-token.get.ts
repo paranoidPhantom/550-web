3f669f00b876faeab50f6276f0eacb10e5bb9219
Andrei <andrei.hudalla@outlook.com>
2024-06-04 18:53:50 -0500

    Add error handling to test token validation

--- server/api/public/_validate-test-token.get.ts
+++ server/api/public/_validate-test-token.get.ts
@@ -1,12 +1,15 @@
-import { serverSupabaseServiceRole } from '#supabase/server'
+import { serverSupabaseServiceRole } from "#supabase/server";
 
-export default defineEventHandler(
-	async (event) => {
-		const { token } = getQuery(event)
-		if (token) {
-			const supabase = await serverSupabaseServiceRole(event)
-			const { data, error } = await supabase.from("tester-tokens").select().eq("token", token)
-			return data ?? []
-		} else return []
-	}
-);
+export default defineEventHandler(async (event) => {
+    const { token } = getQuery(event);
+    if (token) {
+        const supabase = serverSupabaseServiceRole(event);
+
+        const { data, error } = await supabase
+            .from("tester-tokens")
+            .select()
+            .eq("token", token);
+        if (error) return createError(error);
+        return data ?? [];
+    } else return [];
+});
